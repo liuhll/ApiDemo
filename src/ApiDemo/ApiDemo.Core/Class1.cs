f6c63b2c8003ee3a7a6e12beaaa163d18ef49a35
Hongliang.Liu <1029765111@qq.com>
2017-05-19 09:04:38 +0800

    Init Project

---
+++ src/ApiDemo/ApiDemo.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiDemo.Core
+{
+    public class Class1
+    {
+    }
+}
